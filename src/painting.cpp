525ea123b2bdd844fb2c24c59120ec2bfa5c59ec
yunchengzhang1 <yunchengz@smu.edu>
2021-08-29 13:40:34 -0500

    makelist change

---
+++ src/painting.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AZ on 8/25/2021.
+//
+
+#include "painting.h"
---
+++ src/painting.h
@@ -0,0 +1,18 @@
+//
+// Created by AZ on 8/25/2021.
+//
+
+#ifndef INC_21F_PA01_ARTHUR_ZHANG_PAINTING_H
+#define INC_21F_PA01_ARTHUR_ZHANG_PAINTING_H
+
+
+class painting {
+
+    int price, x, y, ID, ratio;
+
+
+
+};
+
+
+#endif //INC_21F_PA01_ARTHUR_ZHANG_PAINTING_H
