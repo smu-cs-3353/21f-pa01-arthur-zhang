1f10a73c30a5f55e8f13652369d237e105709bf2
Christopher Deng Jr <yunchengz@smu.edu>
2021-09-08 19:02:53 -0500

    command line update

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,21 @@ int main(int argc, char** const argv) {
     string filename = "test/";
     filename+=argv[1];
     ifstream file_input(filename);
-    ofstream brute_force("test-bruteforce.txt");
-    ofstream high_value("test-highvalue.txt");
-    ofstream custom ("test-custom.txt");
-    manager manage;
-    manage.run(file_input, brute_force, high_value, custom);
+    if(file_input.is_open())
+    {
+        ofstream brute_force("test-bruteforce.txt");
+        ofstream high_value("test-highvalue.txt");
+        ofstream custom ("test-custom.txt");
+        manager manage;
+        manage.run(file_input, brute_force, high_value, custom);
 
-    file_input.close();
-    brute_force.close();
-    high_value.close();
-    custom.close();
+        file_input.close();
+        brute_force.close();
+        high_value.close();
+        custom.close();
+    }
+    else
+        cout<<"wrong file name"<<endl;
     return 0;
 }
 
